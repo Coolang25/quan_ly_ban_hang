3f14c7d98d21b1c9fbf09ae5001afe978f4fc492
Quat Trinh <quattrinh25111999@gmail.com>
2023-11-29 16:25:31 +0700

    init project

--- src/main/java/com/t3h/quan_ly_ban_hang/repository/BillRepo.java
+++ src/main/java/com/t3h/quan_ly_ban_hang/repository/BillRepo.java
@@ -1,7 +1,9 @@
 package com.t3h.quan_ly_ban_hang.repository;
 
 import com.t3h.quan_ly_ban_hang.entities.Bill;
+import com.t3h.quan_ly_ban_hang.entities.Products;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -11,4 +13,13 @@ public interface BillRepo extends JpaRepository<Bill, Long> {
     List<Bill> findAllByStatusIn(List<Integer> statuses);
 
     List<Bill> findAllByStatusOrderByCreatedTimeDesc(int status);
+
+//    @Query(value = "select * from bill as b where DATE(b.created_time) = CURDATE() and b.status = 3", nativeQuery = true)
+//        List<Bill> findAllSoldDuringTheDay();
+//
+//    @Query(value = "select * from bill as b where MONTH(b.created_time) = MONTH(CURDATE()) AND YEAR(b.created_time) = YEAR(CURDATE()) and b.status = 3", nativeQuery = true)
+//    List<Bill> findAllSoldDuringTheMonth();
+//
+//    @Query(value = "select * from bill as b where YEAR(time) = YEAR(CURDATE()) and b.status = 3", nativeQuery = true)
+//    List<Bill> findAllSoldDuringTheYear();
 }
